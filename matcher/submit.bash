bf79ac2cad55caeb31e2f7a41ec93a0adc37173c
Alex Carlin <carlin@ucdavis.edu>
2015-01-13 19:52:31 -0800

    Fixed cst file

--- matcher/submit.bash
+++ matcher/submit.bash
@@ -13,7 +13,7 @@ STRUCTURE=$( awk 'NR=="'${SGE_TASK_ID}'" { print $1 }' combos.txt )
 
 rosetta_scripts.linuxgccrelease @ design.flags \
   -in:file:s $STRUCTURE \
-  -extra_res_fa in/${STRUCTURE:0:4}.params \
+  -extra_res_fa ${STRUCTURE:0:7}.params \
   -out:path:all out \
   -enzdes::cstfile $CST \
   -out:user_tag $CST
